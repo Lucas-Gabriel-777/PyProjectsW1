9d9c01ee8350a377fb85fb2eb87a50f9071acc3d
Lucas Gabriel <contabtclucas@gmail.com>
2025-05-02 16:52:57 -0300

    Soma de pares

--- 4_Laços de repetição usando for/Py-Ex50.py
+++ 4_Laços de repetição usando for/Py-Ex50.py
@@ -2,3 +2,15 @@
 #Data: 01-05-2025
 #Objetivo: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. 
 # Se o valor digitado for ímpar, desconsidere-o.
+
+total = 0
+lista = []
+
+for c in range (1,7):
+    numero = int(input("Digite o {}º número: ".format(c)))
+    if numero % 2 == 0:
+        total += numero
+        lista.append(numero)
+        
+print("Os números pares que você digitou são {}".format(lista))
+print("A soma dos números pares que voceê digitou resulta em {}".format(total))
