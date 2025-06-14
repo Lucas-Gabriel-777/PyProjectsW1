1df04660d76d14567505b0ba3fd2d82c7f277ea1
Lucas Gabriel <contabtclucas@gmail.com>
2025-06-14 17:12:01 -0300

    Tabuada

---
+++ 4_Laços de repetição usando for/Laço (WHILE)/Py-Ex67.py
@@ -0,0 +1,19 @@
+#Autor: Lucas Gabriel dos Santos Lima
+#Data: 14-06-2025
+#Objetivo: Faça um programa que mostre a tabuada de vários números, 
+# um de cada vez, para cada valor digitado pelo usuário. 
+# O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+    c = 0
+    num = int(input("\nVocê deseja ver a tabuada de qual número?  "))
+    if num <= 0:
+        break
+    print("_"*20)
+    print(" Tabuada do número {}".format(num))
+    print("_"*20)
+
+    while c <= 10:
+        mult = num * c
+        print("{} X {} = {}".format(num, c, mult))
+        c +=1
