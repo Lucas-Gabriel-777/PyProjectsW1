e3014eed847809893b97c2d6b00f8f355251db67
Lucas Gabriel <contabtclucas@gmail.com>
2025-06-19 18:09:09 -0300

    Tratamento simples de dados

--- 5_Tuplas/Py-Ex75.py
+++ 5_Tuplas/Py-Ex75.py
@@ -1,5 +1,5 @@
 #Autor: Lucas Gabriel dos Santos Lima
-#Data: 17-06-2025
+#Data: 19-06-2025
 #Objetivo: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
 # A) Quantas vezes apareceu o valor 9.
 # B) Em que posição foi digitado o primeiro valor 3.
